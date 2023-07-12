e2d033a198927be7b503899c80c4c5810766327d
Fanis <f.shigapov@inbox.ru>
2023-07-12 20:51:07 +0600

    Home work 2

---
+++ Program_Hw2.cs
@@ -0,0 +1,51 @@
+﻿//Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+// Console.Write("Введите трехзначное чило: ");
+// string number = Console.ReadLine();
+// int a = number[number.Length -2] - '0';
+// Console.Write("Вторая цифра введенного чила = ");
+// Console.Write(a);
+
+//Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+// Console.Write("Введи число: ");
+// int anyNumber = Convert.ToInt32(Console.ReadLine());
+// string anyNumberText = Convert.ToString(anyNumber);
+// if (anyNumberText.Length > 2){
+//   Console.WriteLine("Третья цифра " + anyNumberText[2]);
+// }
+// else {
+//   Console.WriteLine("Третьей цифры нет");
+// }
+
+//Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+// Console.Write("Введите номер дня недели: ");
+// int day = Convert.ToInt32(Console.ReadLine());
+
+// if (day == 1) 
+// {
+//     Console.WriteLine("Нет");
+// }
+//     else if (day == 2) 
+//     {
+//         Console.WriteLine("Нет");
+//     }
+//     else if (day == 3) 
+//     {
+//         Console.WriteLine("Нет");
+//     }
+//     else if (day == 4) 
+//     {
+//         Console.WriteLine("Нет");
+//     }
+//     else if (day == 5) 
+//     {
+//         Console.WriteLine("Нет");
+//     }
+//     else if (day == 6) 
+//     {
+//         Console.WriteLine("Да");
+//     }
+//     else if (day == 7) 
+//     {
+//         Console.WriteLine("Да");
+//     }
